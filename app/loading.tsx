ecbcdf6adf7dfa02c1113c83ebcd502f0ff87fab
Ananthu M A <heartchaserananthu@gmail.com>
2025-04-20 21:53:15 +0530

    fix: Updated Loading Component Image Size; Increased Dimensions for Better Visibility

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Loading() {
   return (
     <div className="flex justify-center items-center mt-20 transition animate-pulse">
-      <Image src={`/logo.png`} alt={`Logo`} width={`250`} height={`250`} />
+      <Image src={`/logo.png`} alt={`Logo`} width={`500`} height={`500`} />
     </div>
   );
 }
