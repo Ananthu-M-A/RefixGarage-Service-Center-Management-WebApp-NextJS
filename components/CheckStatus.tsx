4ebb2ab6f333150054fb7730f44ea0a99b916d2c
Ananthu M A <heartchaserananthu@gmail.com>
2025-05-07 20:13:59 +0530

    Refactored CheckStatus Component Layout and Improve UI Consistency

--- components/CheckStatus.tsx
+++ components/CheckStatus.tsx
@@ -80,77 +80,83 @@ function CheckStatus() {
   return (
     <section
       id="check-status"
-      className="w-full px-4 min-h-screen flex flex-wrap gap-10 justify-center items-center bg-transparent text-white"
+      className="w-full px-4 min-h-screen flex flex-col lg:flex-row gap-10 justify-center items-center bg-transparent text-white"
     >
-      <div className="bg-gray-800 w-full max-w-md border border-gray-700 rounded-lg shadow-lg p-6">
-        <div className="mb-6">
-          <h2 className="text-center text-2xl font-bold text-white">
+      <div className="bg-gray-800 w-full max-w-md border border-gray-700 rounded-lg shadow-lg p-6 flex flex-col justify-between h-auto lg:h-[400px]">
+        <div>
+          <h3 className="text-3xl font-bold text-center mb-4">
             Check Service Status
-          </h2>
+          </h3>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your name in capital letters"
+                        {...field}
+                        className="bg-gray-900 text-white"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="mobile"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Mobile Number</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your mobile number"
+                        {...field}
+                        className="bg-gray-900 text-white"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="w-full bg-blue-600 text-white hover:bg-blue-700 px-4 rounded-md hover:cursor-pointer"
+              >
+                Know Service Status
+              </Button>
+            </form>
+          </Form>
         </div>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Enter your name in capital letters"
-                      {...field}
-                      className="bg-gray-900 text-white"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="mobile"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Mobile Number</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Enter your mobile number"
-                      {...field}
-                      className="bg-gray-900 text-white"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button
-              type="submit"
-              className="w-full bg-blue-600 text-white hover:bg-blue-700 px-4 rounded-md hover:cursor-pointer"
-            >
-              Know Service Status
-            </Button>
-          </form>
-        </Form>
       </div>
-      <div className="bg-gray-800 w-full max-w-md border border-gray-700 rounded-lg shadow-lg p-6">
-        <h3 className="text-3xl font-bold text-center mb-4">How To Check</h3>
-        <p className="text-center mb-4 text-lg">
-          Kindly enter both the mobile number and name you provided during the
-          registration process, which were also used to send you a WhatsApp
-          message at that time. This information is required to verify the
-          current service status of your smartphone.
-        </p>
-        <p className="text-center font-bold text-lg mb-2">Team Refix Garage</p>
-        <p className="text-center text-sm text-gray-400 mb-4">
-          If you have any questions, please contact us at{" "}
-          <a
-            href={`tel:${process.env.NEXT_PUBLIC_CONTACT_NUMBER}`}
-            className="text-blue-500 hover:underline"
-          >
-            {process.env.NEXT_PUBLIC_CONTACT_NUMBER}
-          </a>
-        </p>
+      <div className="bg-gray-800 w-full max-w-md border border-gray-700 rounded-lg shadow-lg p-6 flex flex-col justify-between h-auto lg:h-[400px]">
+        <div>
+          <h3 className="text-3xl font-bold text-center mb-4">How To Check</h3>
+          <p className="text-center mb-4 text-lg">
+            Kindly enter both the mobile number and name you provided during the
+            registration process, which were also used to send you a WhatsApp
+            message at that time. This information is required to verify the
+            current service status of your smartphone.
+          </p>
+        </div>
+        <div>
+          <p className="text-center font-bold text-lg mb-2">
+            Team Refix Garage
+          </p>
+          <p className="text-center text-sm text-gray-400">
+            If you have any questions, please contact us at{" "}
+            <a
+              href={`tel:${process.env.NEXT_PUBLIC_CONTACT_NUMBER}`}
+              className="text-blue-500 hover:underline"
+            >
+              {process.env.NEXT_PUBLIC_CONTACT_NUMBER}
+            </a>
+          </p>
+        </div>
       </div>
     </section>
   );
